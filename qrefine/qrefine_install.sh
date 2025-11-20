a17336fc2b89622d41aa8812c880e4de9653042b
Brandon Case <casebw@umich.edu>
2025-11-20 12:53:50 -0500

    add qrefine folder and change alias to function

--- qrefine/qrefine_install.sh
+++ qrefine/qrefine_install.sh
@@ -31,9 +31,17 @@ echo "export NUMBA_CUDA_USE_NVIDIA_BINDING=1" >> ~/.bashrc
 conda install -y torchani -c conda-forge
 
 
-#create alias to set up environment for using the package
-echo "alias qrefine='module load mamba/py3.13; source activate qrefine; $source_cmd'" >> ~/.bashrc
+#create qrefine function to set up environment for using the package
+cat << EOF >> ~/.test1
+function qrefine {
+        module load mamba/py3.13
+        source activate qrefine
+        source /home/casebw/.conda/envs/qrefine/lib/python3.10/site-packages/build/setpaths.sh
+}
+export -f qrefine
+EOF
 
 
-echo "An alias has been created in your ~/.bashrc to load the necessary modules and environment for qrefine.  The alias can be called as 'qrefine'."
-echo "You will need to log out and back in or re-load your .bashrc file with the command 'source ~/.bashrc' to have access to the new alias."
+
+echo "A function has been created in your ~/.bashrc to load the necessary modules and environment for qrefine.  It can be called as 'qrefine'."
+echo "You will need to log out and back in or re-load your .bashrc file with the command 'source ~/.bashrc' to have access to the new function."
